dc31f4e5e4f687cb26f428b931057f5cebf9e474
haris91011 <mahris2921@gmail.com>
2024-12-25 20:44:05 +0500

    update file path

--- data/data_retreival.py
+++ data/data_retreival.py
@@ -1,23 +1,24 @@
-import json
-import os
-
-def get_questions(stage):
-    # Get the absolute path to ensure compatibility
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_path, "data", "questions.json")
-
-    # Read the JSON file
-    with open(file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-
-    return data[stage]
-
-
-# import json 
+# import json
+# import os
 
 # def get_questions(stage):
-    
-#     with open("data\questions.json", "r") as file:
+#     # Get the absolute path to ensure compatibility
+#     base_path = os.path.dirname(os.path.abspath(__file__))
+#     file_path = os.path.join(base_path, "data", "questions.json")
+
+#     # Read the JSON file
+#     with open(file_path, "r", encoding="utf-8") as file:
+#         print("File path:", file_path)
 #         data = json.load(file)
 
 #     return data[stage]
+
+
+import json 
+
+def get_questions(stage):
+    
+    with open(r"data\questions.json", "r") as file:
+        data = json.load(file)
+
+    return data[stage]
